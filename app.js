648dac5f3a4bd9c83e424f5958a3e1e50ba11d2c
Svitlana Revniuk <69059791+Revniuk@users.noreply.github.com>
2024-01-18 12:31:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Setting the view engine to Pug
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// Routing to the first view
+app.get('/', (req, res) => {
+  res.render('view1');
+});
+
+// Routing to the second view
+app.get('/view2', (req, res) => {
+  res.render('view2');
+});
+
+// Routing with a form
+app.post('/submit', (req, res) => {
+  // Log form data to console
+  console.log('Received form data:', req.body);
+
+  // Sending  a simple success response
+  res.send('Form submitted successfully!');
+});
+
+// Routing  with a parameter
+app.get('/dynamic/:param', (req, res) => {
+  const dynamicParam = req.params.param;
+  res.send(`Dynamic parameter: ${dynamicParam}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});

ae6cc9c98208c346830ef13e48948e72f032a6d1
Svitlana Revniuk <69059791+Revniuk@users.noreply.github.com>
2024-01-18 15:17:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path'); // Add this line for path module
 const app = express();
 const port = 3000;
 
-// Middleware for logging using morgan
 app.use(morgan('dev'));
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-
-// Set the view engine to Pug
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
-
-// Routing to the first view
-app.get('/', (req, res) => {
-  res.render('view1');
+app.get('/download', (req, res) => {
+  const filePath = path.join(__dirname, 'public', 'image.jpg');
+  res.download(filePath, 'image.jpg');
 });
 
-// Routing to the second view
 app.get('/view2', (req, res) => {
   res.render('view2');
 });
 
-// Routing with a form
 app.post('/submit', (req, res) => {
-  // Log form data to console
   console.log('Received form data:', req.body);
-
-  // Sending  a simple success response
   res.send('Form submitted successfully!');
 });
 
-// Routing  with a parameter
 app.get('/dynamic/:param', (req, res) => {
   const dynamicParam = req.params.param;
   res.send(`Dynamic parameter: ${dynamicParam}`);
